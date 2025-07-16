555c5bd0192e02040107a8c2d803ea8f11adcd8d
Yeray500 <yeray500@gmail.com>
2025-07-16 02:28:26 +0200

    Multiplo-de-2-ejercicio3

---
+++ Multiplo-de-2.py
@@ -0,0 +1,8 @@
+# Pedimos al usuario que ingrese un número
+numero = int(input("Ingresa un número: "))
+
+# Verificamos si es múltiplo de 2
+if numero % 2 == 0:
+    print("El número ES múltiplo de 2")
+else:
+    print("El número NO es múltiplo de 2")
